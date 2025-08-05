9df95d9afadbb2f90c98c5ff8fca73f3c4f58f2a
karansingla007 <karansingla120@gmail.com>
2025-08-05 22:11:17 +0530

    Test + Impl: multiple numbers and newline delimiter

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -9,3 +9,9 @@ def test_single_number():
 
 def test_two_numbers():
     assert add("1,2") == 3
+
+def test_multiple_numbers():
+    assert add("1,2,3,4,5") == 15
+
+def test_newlines_between_numbers():
+    assert add("1\n2,3") == 6
